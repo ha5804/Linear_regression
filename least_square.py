d5429f5ec68c84db3b7ce7b398767ec2f97b7d86
ha5804 <zixpotf5803@gmail.com>
2025-07-03 11:01:34 +0900

    data를 저장하자.

--- least_square.py
+++ least_square.py
@@ -11,4 +11,8 @@ class MyData:
     def __init__(self):
         self.filename_point = "./data/point.csv"
         #유지보수 위해 데이터를 class의 속성으로 저장해놓자.
-        
+
+    def get_point(self):
+        point = np.genfromtxt(self.filename_point, delimiter = ',')
+        return point
+
