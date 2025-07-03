b9edfa50a52598006933c71436fd105118b687d3
ha5804 <zixpotf5803@gmail.com>
2025-07-03 11:00:23 +0900

    필요 라이브러리 임포트

---
+++ least_square.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from matplotlib.ticker import LinearLocator
+from matplotlib import _cm
+from mpl_toolkits.mplot3d import Axes3D
+import csv
+
+class MyData:
+    def __init__(self):
+        self.filename_point = "./data/point.csv"
+        #유지보수 위해 데이터를 class의 속성으로 저장해놓자.
+        
